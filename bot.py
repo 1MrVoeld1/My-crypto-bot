098f419493cb75a118b2510bc977cbfd503f75f6
1MrVoeld1 <uliolaser64@gmail.com>
2025-11-14 17:01:57 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+import os  # чтобы брать токен из переменных окружения
+from telegram.ext import Updater, CommandHandler
 
+# Получаем токен из переменной окружения
+TOKEN = os.getenv("7816687479:AAEurje4CWqERb1aJPtzFPfcINqbylRqLrc")
+
+# Функция, которая отвечает на команду /start
+def start(update, context):
+    update.message.reply_text("Бот запущен и работает!")
+
+# Основная часть: создаём бота и запускаем его
+if _name_ == "_main_":
+    # Создаём updater для работы с ботом
+    updater = Updater(TOKEN)
+    
+    # Привязываем команду /start к функции start
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    
+    # Запускаем бота
+    updater.start_polling()
+    updater.idle()
